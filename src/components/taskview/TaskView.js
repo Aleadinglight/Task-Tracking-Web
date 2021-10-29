eadd0a7a6dec414bd4d7cf8c4dbed40f422b0004
Tam Nguyen <tamnguyen@excosoft.fi>
2021-10-29 03:34:48 +0300

    Update some basic components to project.

---
+++ src/components/taskview/TaskView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TaskView extends Component {
+  render() {
+    return (
+      <div>
+        This is the task view
+      </div>
+    )
+  }
+}
