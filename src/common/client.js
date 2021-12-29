9bf9a3df388b2ad53c6b69008e074b5d7f3625b3
Tam Nguyen <tamnguyen@excosoft.fi>
2021-12-29 14:09:28 +0200

    added client.js

--- src/common/client.js
+++ src/common/client.js
@@ -2,5 +2,15 @@ import axios from 'axios'
 require('dotenv').config()
 
 let loginUser = async (credentials) => {
-  return axios.post(URL, )
+  try {
+    return await axios.post(URL, credentials);
+  }
+  catch (err) {
+    console.log(`Error when trying to log in. ${err}`)
+    return null;
+  }
+}
+
+module.exports = {
+  loginUser
 }
