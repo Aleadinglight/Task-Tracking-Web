1175054d0e2cb7cddf6abd45e1225f2fb68e0ed2
DESKTOP-G3UMIE3\huuta <huutam.ptnk@gmail.com>
2022-02-22 08:32:11 +0200

    Convert NewTask component to functional approach

--- src/components/newtask/NewTask.js
+++ src/components/newtask/NewTask.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
@@ -6,190 +7,196 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import "./NewTask.css";
 
-export default class NewTask extends Component {
-  constructor(props) {
-    super(props);
+export default function NewTask(props) {
+  const [task, setTask] = useState({
+    name: "",
+    description: "",
+    priority: 0,
+    status: 0,
+    due: ""
+  });
+  const {
+    show,
+    onHide,
+    inputTask
+  } = props;
+  // updateProps = (task) => {
+  //   if (!task) {
+  //     return;
+  //   }
 
-    this.state = {
-      task: {
-        name: "",
-        description: "",
-        priority: 0,
-        status: 0,
-        due: ""
-      }
-    };
-  }
+  //   setState({
+  //     task: {
+  //       name: task.name,
+  //       description: task.description,
+  //       priority: task.priority,
+  //       status: task.status,
+  //       due: task.due
+  //     }
+  //   })
+  // }
 
-  updateProps = (task) => {
-    if (!task) {
-      return;
-    }
-
-    this.setState({
-      task: {
-        name: task.name,
-        description: task.description,
-        priority: task.priority,
-        status: task.status,
-        due: task.due
-      }
-    })
-  }
-
-  getTaskName = () => {
-    if (!this.props.props) {
+  const getTaskName = () => {
+    if (!inputTask) {
       return "";
     }
-    else {
-      return this.props.props.name;
-    }
-  }
 
-  getTaskDescription = () => {
-    if (!this.props.props) {
+    return inputTask.name;
+  };
+
+  const getTaskDescription = () => {
+    if (!inputTask) {
       return "";
     }
-    else {
-      return this.props.props.description;
-    }
-  }
 
-  getTaskPriority = () => {
-    if (!this.props.props) {
-      return "";
-    }
-    else {
-      return this.props.props.priority;
-    }
-  }
+    return inputTask.description;
+  };
 
-  getTaskStatus = () => {
-    if (!this.props.props) {
-      return "";
+  const getTaskPriority = () => {
+    if (!inputTask) {
+      return 0;
     }
-    else {
-      console.log(`this.props.props.status`, this.props.props.status)
-      return this.props.props.status;
-    }
-  }
+    return inputTask.priority;
+  };
 
-  getTaskDate = () => {
-    if (!this.props.props) {
-      return "";
+  const getTaskStatus = () => {
+    if (!inputTask) {
+      return 0;
     }
-    else {
-      console.log(`this.props.props.due`, this.props.props.due)
-      let date = new Date(this.props.props.due.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+    return inputTask.status;
+  };
+
+  const getTaskDate = () => {
+    if (!inputTask) {
+      const date = new Date(Date.now());
       const offset = date.getTimezoneOffset();
-      let trueDate = new Date(date.getTime() - (offset * 60 * 1000));
+      const trueDate = new Date(date.getTime() - (offset * 60 * 1000));
       return trueDate.toISOString().split("T")[0];
     }
-  }
 
-  render() {
-    return (
-      <Modal
-        {...this.props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <h4><b>New Task</b></h4>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3">
-              <Form.Label>Task Name</Form.Label>
-              <Form.Control placeholder="Enter task name"
-                defaultValue={this.getTaskName()}
-                onChange={(e) => this.setState({
-                  task: {
-                    ...this.state.task,
-                    name: e.target.value
-                  }
-                })}
-              />
-            </Form.Group>
+    const date = new Date(inputTask.due.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+    const offset = date.getTimezoneOffset();
+    const trueDate = new Date(date.getTime() - (offset * 60 * 1000));
+    return trueDate.toISOString().split("T")[0];
+  };
 
-            <Form.Group className="mb-3" >
-              <Form.Label>Description</Form.Label>
-              <Form.Control placeholder="Description of this task"
-                as="textarea"
-                defaultValue={this.getTaskDescription()}
-                onChange={(e) => this.setState({
-                  task: {
-                    ...this.state.task,
-                    description: e.target.value
-                  }
-                })}
-              />
-            </Form.Group>
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <h4><b>New Task</b></h4>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Task Name</Form.Label>
+            <Form.Control
+              placeholder="Enter task name"
+              defaultValue={getTaskName()}
+              onChange={(e) => setTask({
+                ...task,
+                name: e.target.value
+              })}
+            />
+          </Form.Group>
 
-            <Form.Group className="mb-3">
-              <Row>
-                <Col>
-                  <Form.Label>Deadline</Form.Label>
-                  <Form.Control type="date"
-                    defaultValue={this.getTaskDate()}
-                    onChange={(e) => this.setState({
-                      task: {
-                        ...this.state.task,
-                        due: e.target.value
-                      }
-                    })} />
-                </Col>
-                <Col>
-                  <Form.Label>Priority</Form.Label>
+          <Form.Group className="mb-3">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              placeholder="Description of this task"
+              as="textarea"
+              defaultValue={getTaskDescription()}
+              onChange={(e) => setTask({
+                ...task,
+                description: e.target.value
+              })}
+            />
+          </Form.Group>
 
-                  <Form.Select
-                    label="Priority"
-                    defaultValue={this.getTaskPriority()}
-                    onChange={(e) => this.setState({
-                      task: {
-                        ...this.state.task,
-                        priority: e.target.value
-                      }
-                    })}>
-                    <option value={0}>High</option>
-                    <option value={1}>Medium</option>
-                    <option value={2}>Low</option>
-                  </Form.Select>
+          <Form.Group className="mb-3">
+            <Row>
+              <Col>
+                <Form.Label>Deadline</Form.Label>
+                <Form.Control
+                  type="date"
+                  defaultValue={getTaskDate()}
+                  onChange={(e) => setTask({
+                    ...task,
+                    due: e.target.value
+                  })}
+                />
+              </Col>
+              <Col>
+                <Form.Label>Priority</Form.Label>
 
-                </Col>
-                <Col>
-                  <Form.Label>Status</Form.Label>
-                  <Form.Select
-                    defaultValue={this.getTaskStatus()}
-                    onChange={(e) => this.setState({
-                      task: {
-                        ...this.state.task,
-                        status: e.target.value
-                      }
-                    })}>
-                    <option value={0}>On-Going</option>
-                    <option value={1}>Open</option>
-                    <option value={2}>Completed</option>
-                  </Form.Select>
-                </Col>
-              </Row>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            onClick={() => this.props.onHide(this.state.task)}>
-            Update
-          </Button>
-          <Button variant="secondary"
-            onClick={() => this.props.onHide(false)}>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+                <Form.Select
+                  label="Priority"
+                  defaultValue={getTaskPriority()}
+                  onChange={(e) => setTask({
+                    ...task,
+                    priority: e.target.value
+                  })}
+                >
+                  <option value={0}>High</option>
+                  <option value={1}>Medium</option>
+                  <option value={2}>Low</option>
+                </Form.Select>
+
+              </Col>
+              <Col>
+                <Form.Label>Status</Form.Label>
+                <Form.Select
+                  defaultValue={getTaskStatus()}
+                  onChange={(e) => setTask({
+                    ...task,
+                    status: e.target.value
+                  })}
+                >
+                  <option value={0}>On-Going</option>
+                  <option value={1}>Open</option>
+                  <option value={2}>Completed</option>
+                </Form.Select>
+              </Col>
+            </Row>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          onClick={() => onHide(task)}
+        >
+          Update
+        </Button>
+        <Button
+          variant="secondary"
+          onClick={() => onHide(false)}
+        >
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
+
+NewTask.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
+  inputTask: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    status: PropTypes.number,
+    priority: PropTypes.number,
+    due: PropTypes.node,
+  })
+};
+
+NewTask.defaultProps = {
+  inputTask: null,
+};
