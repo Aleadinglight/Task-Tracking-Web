df5017861180e2ffa38b2d92f3c573c5f5a3c1cd
DESKTOP-G3UMIE3\huuta <huutam.ptnk@gmail.com>
2022-02-22 02:53:01 +0200

    Refactor Menu component and TaskSummary component from class to pure function.

--- src/components/mymenu/MyMenu.js
+++ src/components/mymenu/MyMenu.js
@@ -1,86 +1,86 @@
-import "./MyMenu.css"
-import React, { Component } from 'react'
-import { Stack } from "react-bootstrap"
+import "./MyMenu.css";
+import React from 'react';
+import { Stack } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChartArea, faCog, faClipboardList, faCalendarAlt, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChartArea, faCog, faClipboardList, faCalendarAlt, faInfoCircle
+} from '@fortawesome/free-solid-svg-icons';
 
-export default class MyMenu extends Component {
+export default function MyMenu() {
+  const activeStyle = {
+    color: '#3080d0',
+    backgroundColor: '#e5e8ec'
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeStyle: {
-        color: '#3080d0',
-        backgroundColor: '#e5e8ec'
-      }
-    }
-  }
-
-  checkActive = (match, location) => {
+  const checkActive = (match) => {
     if (!match) {
       return false;
     }
     return match.isExact;
-  }
+  };
 
-  render() {
-    return (
-      <div className="my-menu">
-        <Stack>
+  return (
+    <div className="my-menu">
+      <Stack>
 
-          <div className="my-menu-header">
-            <h4 className="my-menu-header-text">
-              {"_tasktracking"}
-            </h4>
-          </div>
+        <div className="my-menu-header">
+          <h4 className="my-menu-header-text">
+            _tasktracking
+          </h4>
+        </div>
 
-          <NavLink className="my-menu-item" to="/"
-            isActive={this.checkActive}
-            activeStyle={this.state.activeStyle}
-          >
-            <FontAwesomeIcon icon={faChartArea} />
-            {" "}
-            <b>Dash Board</b>
-          </NavLink>
-          <NavLink className="my-menu-item"
-            to="/tasks" 
-            isActive={this.checkActive}
-            activeStyle={this.state.activeStyle}
-          >
-            <FontAwesomeIcon icon={faClipboardList} />
-            {" "}
-            <b>My Tasks</b>
-          </NavLink>
-          <NavLink className="my-menu-item"
-            to="/settings"
-            isActive={this.checkActive}
-            activeStyle={this.state.activeStyle}
-          >
-            <FontAwesomeIcon icon={faCog} />
-            {" "}
-            <b>User Settings</b>
-          </NavLink>
-          <NavLink className="my-menu-item"
-            to="/calendar"
-            isActive={this.checkActive}
-            activeStyle={this.state.activeStyle}
-          >
-            <FontAwesomeIcon icon={faCalendarAlt} />
-            {" "}
-            <b>My Calendar</b>
-          </NavLink>
-          <NavLink className="my-menu-item"
-            to="/about"
-            isActive={this.checkActive}
-            activeStyle={this.state.activeStyle}
-          >
-            <FontAwesomeIcon icon={faInfoCircle} />
-            {" "}
-            <b>About</b>
-          </NavLink>
-        </Stack>
-      </div >
-    )
-  }
+        <NavLink
+          className="my-menu-item"
+          to="/"
+          isActive={checkActive}
+          activeStyle={activeStyle}
+        >
+          <FontAwesomeIcon icon={faChartArea} />
+          {" "}
+          <b>Dash Board</b>
+        </NavLink>
+        <NavLink
+          className="my-menu-item"
+          to="/tasks"
+          isActive={checkActive}
+          activeStyle={activeStyle}
+        >
+          <FontAwesomeIcon icon={faClipboardList} />
+          {" "}
+          <b>My Tasks</b>
+        </NavLink>
+        <NavLink
+          className="my-menu-item"
+          to="/settings"
+          isActive={checkActive}
+          activeStyle={activeStyle}
+        >
+          <FontAwesomeIcon icon={faCog} />
+          {" "}
+          <b>User Settings</b>
+        </NavLink>
+        <NavLink
+          className="my-menu-item"
+          to="/calendar"
+          isActive={checkActive}
+          activeStyle={activeStyle}
+        >
+          <FontAwesomeIcon icon={faCalendarAlt} />
+          {" "}
+          <b>My Calendar</b>
+        </NavLink>
+        <NavLink
+          className="my-menu-item"
+          to="/about"
+          isActive={checkActive}
+          activeStyle={activeStyle}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          {" "}
+          <b>About</b>
+        </NavLink>
+      </Stack>
+    </div>
+  );
 }
--- src/components/tasksummary/TaskSummary.js
+++ src/components/tasksummary/TaskSummary.js
@@ -1,66 +1,66 @@
 import './TaskSummary.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Navbar from 'react-bootstrap/Navbar';
 import MyChart from '../mychart/MyChart';
 
-export default class TaskSummary extends Component {
-  render() {
-    return (
-      <div className="dashboard-content-container">
-        <Navbar className="my-navbar" >
-          <Container>
-            <Navbar.Brand ></Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                Signed in as: <a href="#login">Admin</a>
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-
-        <Container className="dashboard-content" fluid>
-          <h2 className="dashboard-header">Dashboard</h2>
-          <Card className="summary-card">
-            <Card.Header><b>On-going</b></Card.Header>
-            <Card.Body>
-              <Card.Text className="summary-card-title">
-                3
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card className="summary-card">
-            <Card.Header><b>Completed</b></Card.Header>
-            <Card.Body>
-              <Card.Text className="summary-card-title">
-                65
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card className="summary-card">
-            <Card.Header><b>Open</b></Card.Header>
-            <Card.Body>
-              <Card.Text className="summary-card-title">
-                5
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card className="summary-card">
-            <Card.Header><b>Overdue</b></Card.Header>
-            <Card.Body>
-              <Card.Text className="summary-card-title">
-                2
-              </Card.Text>
-            </Card.Body>
-          </Card>
+export default function TaskSummary() {
+  return (
+    <div className="dashboard-content-container">
+      <Navbar className="my-navbar">
+        <Container>
+          <Navbar.Brand />
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as:
+              {' '}
+              <a href="#login">Admin</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
         </Container>
-        <MyChart />
-      </div>
-    )
-  }
+      </Navbar>
+
+      <Container className="dashboard-content" fluid>
+        <h2 className="dashboard-header">Dashboard</h2>
+        <Card className="summary-card">
+          <Card.Header><b>On-going</b></Card.Header>
+          <Card.Body>
+            <Card.Text className="summary-card-title">
+              3
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="summary-card">
+          <Card.Header><b>Completed</b></Card.Header>
+          <Card.Body>
+            <Card.Text className="summary-card-title">
+              65
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="summary-card">
+          <Card.Header><b>Open</b></Card.Header>
+          <Card.Body>
+            <Card.Text className="summary-card-title">
+              5
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="summary-card">
+          <Card.Header><b>Overdue</b></Card.Header>
+          <Card.Body>
+            <Card.Text className="summary-card-title">
+              2
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
+      <MyChart />
+    </div>
+  );
 }
